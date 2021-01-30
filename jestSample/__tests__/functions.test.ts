336143b6bfeef21b700de3ef645c85b4d5b522e5
gn-t-k <gntk1995422@gmail.com>
2021-01-30 22:43:51 +0900

    テストを追加

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,4 +1,9 @@
-import { sumOfArray, asyncSumOfArray } from "../functions";
+import {
+  sumOfArray,
+  asyncSumOfArray,
+  asyncSumOfArraySometimesZero,
+} from "../functions";
+import { DatabaseMock } from "../util";
 
 describe("sumOfArray", (): void => {
   test("[1, 1]を渡すと2が返ってくる", (): void => {
@@ -24,6 +29,44 @@ describe("asyncSumOfArray", (): void => {
   test("空の配列を渡すと例外が発生する", (): void => {
     asyncSumOfArray([]).then((actual): void => {
       expect(actual).toThrow();
+    });
+  });
+});
+
+describe("asyncSumOfArraySometimesZero", (): void => {
+  test("[1, 1]を渡すと2が返ってくる", (done): void => {
+    const myDatabaseMock = jest
+      .fn<DatabaseMock, []>()
+      .mockImplementation(() => {
+        return {
+          save: (_arg: number[]) => {},
+        };
+      });
+    const database = new myDatabaseMock();
+
+    const expected = 2;
+    asyncSumOfArraySometimesZero([1, 1], database).then((actual): void => {
+      expect(actual).toEqual(expected);
+      done();
+    });
+  });
+
+  test("例外が発生した場合0が返ってくる", (done): void => {
+    const myDatabaseMock = jest
+      .fn<DatabaseMock, []>()
+      .mockImplementation(() => {
+        return {
+          save: (_arg: number[]) => {
+            throw new Error("fail!");
+          },
+        };
+      });
+    const database = new myDatabaseMock();
+
+    const expected = 0;
+    asyncSumOfArraySometimesZero([1, 1], database).then((actual): void => {
+      expect(actual).toEqual(expected);
+      done();
     });
   });
 });
