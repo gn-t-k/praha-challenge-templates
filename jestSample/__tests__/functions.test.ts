8da0f80ad2a75aa44140b90a8bf2209f179b0d58
gn-t-k <gntk1995422@gmail.com>
2021-01-30 16:31:49 +0900

    テストを追加

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,10 @@
-// todo: ここに単体テストを書いてみましょう！
+import { sumOfArray } from "../functions";
+
+describe("sumOfArray", (): void => {
+  it("[1, 2, 3]を渡すと6が返ってくる", (): void => {
+    const expected = 6;
+    const actual = sumOfArray([1, 2, 3]);
+
+    expect(actual).toEqual(expected);
+  });
+});
